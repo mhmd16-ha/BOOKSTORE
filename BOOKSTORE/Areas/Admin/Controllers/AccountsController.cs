0355b801a7a7dc7415f01ec0b491cf49736d34f0
mhmd16-ha <https://github.com/mhmd16-ha/Todo_List.git>
2022-04-21 13:19:39 +0200

    Add project files.

---
+++ BOOKSTORE/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOOKSTORE.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
